0b75d338f4b7d7e42c4abfcb1af4a6745e17fa37
kkopp <kristina.kopp@gmail.com>
2018-10-04 21:57:22 -0600

    added js code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,11 @@
-    var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
 
     var wins = 0;
     var losses = 0;
     var guesses = 10;
+    var lettersGuessed = [];
+
+    var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
     var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
@@ -10,10 +13,22 @@
 
     document.onkeyup = function(event) {
         var userGuess = event.key;
+        lettersGuessed.push(userGuess);
 
-        if (userGuess === computerChoice){
+            if (userGuess === computerChoice) {
                 wins++;
-        } else {
-            guesses--;
-        }
-    }
+            } else {
+                guesses--;
+            }
+            if (guesses === 0) {
+                losses++;
+            }
+            
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("guesses").innerHTML = guesses;
+    document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+    
+    };
+    
+
